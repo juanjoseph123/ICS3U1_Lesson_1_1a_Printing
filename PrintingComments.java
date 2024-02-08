fe9f05a3ab12207ad9292d40d4f8b62981b661b7
juanjoseph123 <juan.joseph25@ycdsbk12.ca>
2024-02-08 11:46:16 -0500

    Updated Printing comments. java

--- PrintingComments.java
+++ PrintingComments.java
@@ -1,3 +1,5 @@
+import javax.swing.text.html.HTMLDocument.HTMLReader.HiddenAction;
+
 class PrintingComments extends ConsoleProgram {
   /**
   * A printing and comments example
@@ -8,14 +10,33 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
-
+    System.out.println("hi" );
+    System.out.println("there");
     // Say hello on the same line
-    
+    System.out.print("Hi");
+    System.out.print( " There!");
     
     // multiline comment
+    /*
+     * This is a 
+     * multiline
+     * comment
+     */
     
-    
-
+    // Draw a box
+   System.out.println("*************");
+   System.out.println("*           *");
+   System.out.println("*           *");
+   System.out.println("*           *");
+  System.out.println("*************");
+      
+   // Draw another box
+   /*
+   System.out.println("*************");
+   System.out.println("*           *");
+   System.out.println("*           *");
+   System.out.println("*           *");
+   System.out.println("*************");
+   */
   }
 }
